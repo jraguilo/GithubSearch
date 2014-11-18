626d96f30132dd298086cd1fd0ac65202f3084a9
jraguilo <johnreyaguilo@gmail.com>
2014-11-18 11:19:08 -0800

    Enabled paging

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    var url = 'https://api.github.com/search/repositories';
-    var page;
+    var links = {}; 
     
     $('#ghsearch').keypress(function(e){
         if(e.which == 13) {
@@ -11,6 +10,26 @@ $(document).ready(function() {
     $('#ghsubmitbtn').on('click', function(e){
         e.preventDefault();
         searchRepo();      
+    });
+    
+    $('#first').on('click', function(e){
+        e.preventDefault();
+        queryRepo(links['first']);      
+    });
+    
+    $('#prev').on('click', function(e){
+        e.preventDefault();
+        queryRepo(links['prev']);      
+    });
+    
+    $('#next').on('click', function(e){
+        e.preventDefault();
+        queryRepo(links['next']);      
+    });   
+    
+    $('#last').on('click', function(e){
+        e.preventDefault();
+        queryRepo(links['last']);      
     });
 
 
@@ -50,14 +69,19 @@ $(document).ready(function() {
                 order = "asc"
                 break;
             }
+        var url = 'https://api.github.com/search/repositories';
     
         var request = {
             q : search,
             sort : sort,
             order : order,
         };
-
-        $.getJSON(url, request, function(json){
+        queryRepo(url, request);
+    }
+    
+    function queryRepo(url, request) {
+        var ghquery = $.getJSON(url, request, function(json){
+            parseLinkHeader(ghquery.getResponseHeader('link'));
             var repocount = json.total_count;
             var outhtml = '<p>repositories found: ' + repocount + '</p>';
             outhtml = outhtml + '<div><p><strong>Repository List:</strong></p>';
@@ -73,13 +97,13 @@ $(document).ready(function() {
                 outhtml = outhtml + '<p class="repo-desc">' + repodesc + '</p>';
                 outhtml = outhtml + '<p class="repo-counts">forks: ' + forks + ' stargazers: ' + stars + '</p>';
                 outhtml = outhtml + '<p class="repo-updated">' + updated + '</p>';
-        });
-        outhtml = outhtml + '</div>'; 
-        $('#ghapidata').html(outhtml);
-    });    
-    
-  }
+                });
+            outhtml = outhtml + '</div>'; 
+            $('#ghapidata').html(outhtml);
+        });    
+    }
   
+    //formats the updated date from the json output
     function getDateString(updated){
         var datestring;
         var day = updated.substring(8,10);
@@ -87,6 +111,34 @@ $(document).ready(function() {
         var year = updated.substring(0,4);
         datestring = 'last updated ' + month + '-' + day + "-" + year;
         return datestring;
+    }
+    
+    //retrieves links from the link header
+    function parseLinkHeader(header) {
+        if (header.length == 0) {
+            throw new Error("invalid input");
+        }
+        //disable all page buttons
+        $('#first').prop("disabled", true);
+        $('#prev').prop("disabled", true);
+        $('#next').prop("disabled", true);
+        $('#last').prop("disabled", true);
+        //split parts by comma
+        var parts = header.split(',');
+
+        var output = "";
+        //parse each part into a named link
+        $.each(parts, function(index, value){
+            var section = value.split(';');
+            if(section.length != 2) {
+                throw new Error("Header could not be split on ';'");
+            }
+            var linkurl = section[0].replace(/<(.*)>/, '$1').trim();
+            var linkname = section[1].replace(/rel="(.*)"/, '$1').trim();
+            links[linkname] = linkurl;
+            $('#'+linkname).prop("disabled", false); 
+        });
+        $('#pages').removeClass('hidden');
     }
   
   
